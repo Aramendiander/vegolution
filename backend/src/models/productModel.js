0d53e7360ae5aa772f9d1b246fff80ba7974f9c2
Aramendiander <aramendiander@gmail.com>
2023-12-04 17:24:03 +0100

    basic mongoose configuration

---
+++ backend/src/models/productModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+/**
+ * @module models/productModel
+ */
+
+/**
+ * Product model for ecommerce
+ * @typedef {Object} productModel
+ * @property {String} name Name of the product
+ * @property {String} shortdescription Short description of the product
+ * @property {String} longdescription Long description of the product
+ * @property {String} picture URL of the product picture
+ * @property {Number} price Price of the product
+ */
+
+const productSchema = new mongoose.Schema({
+    name: String,
+    shortdescription: String,
+    longdescription: String,
+    picture: String,
+    price: Number,
+});
+
+const productModel = mongoose.model("products", productSchema);
+
+export default productModel;
